029c5d1bc68a1fea6e9052cff6d869d7951cf35e
Eugene Zaikonnikov <eugene@funcall.org>
2016-10-20 14:19:31 +0200

    expanding the wrapper

--- cl-pjsip.lisp
+++ cl-pjsip.lisp
@@ -1,4 +1,4 @@
-;;;; cl-pjsip.lisp
+;;;; Library wrapper
 
 (in-package #:cl-pjsip)
 
@@ -7,3 +7,51 @@
 
 (use-foreign-library libpjsip)
 
+(defctype pj-status-t :int)
+
+(defcfun "pj_init" pj-status-t)
+(defcfun "pj_set_log_level" :void (log-level :int))
+(defcfun "pjlib_util_init" pj-status-t)
+(defcfun "pj_caching_pool_init" :void (caching-pool :pointer) (factory-default-policy :pointer) (flags :int))
+(defcfun "pjsip_endpt_create" pj-status-t (factory :pointer) (endpt-name :string) (endpoint :pointer))
+
+(defcunion pj-in6-addr
+  (s6-addr :uint8 :count 32)
+  (u6-addr32 :uint32 :count 4))
+
+(defcstruct pj-sockaddr-in6
+  (sin6-zero-len :uint8)
+  (sin6-family :uint8)
+  ;;assume zero len
+  ;;(sin6-family :uint16)
+  (sin6-port :uint16)
+  (sin6-flowinfo :uint32)
+  (sin6-addr (:union pj-in6-addr))
+  (sin6-scope-id :uint32))
+
+(defcstruct pj-in-addr
+  (s-addr :uint32))
+
+(defcstruct pj-sockaddr-in
+  (sin-zero-len :uint8)
+  (sin-family :uint8)
+  ;;assume zero len
+  ;;(sin6-family :uint16)
+  (sin-port :uint16)
+  (sin-addr (:struct pj-in-addr))
+  (sin-zero :char :count 8))
+
+(defcunion pj-sockaddr
+  (addr (:struct pj-addr-hdr))
+  (ipv4 (:struct pj-sockaddr-in))
+  (ipv6 (:struct pj-sockaddr-in6)))
+
+(defcfun "pjsip_sockaddr_init" :void (family :int) (addr :pointer) (cp :pointer) (port :uint))
+
+(defcstruct pjsip-host-port
+  (host :string)
+  (port :int))
+
+(defcfun "pjsip_udp_transport_start" pj-status-t (endpoint :pointer) (local-addressptr :pointer)
+	 (hostport (:pointer (:struct pjsip-host-port)))
+	 (async-cnt :unit))
