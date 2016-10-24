1bfaec6345fe593f48d388dedefb0cca287f2350
Eugene Zaikonnikov <eugene@funcall.org>
2016-10-24 14:05:45 +0200

    globals

--- cl-pjsip-ua.lisp
+++ cl-pjsip-ua.lisp
@@ -8,3 +8,23 @@
 (use-foreign-library libpjmedia-codec)
 (use-foreign-library libpjlib-util)
 
+(defconstant +sip-port+ 5060)
+(defconstant +rtp-port+ 4000)
+(defconstant +max-media-cnt+ 2)
+
+(defparameter *complete* (convert-to-foreign 0 'pj-bool))
+(defparameter *endpt* (null-pointer))
+(defvar *cp* (foreign-alloc '(:struct pj-caching-pool)))
+(defparameter *med-endpt* (null-pointer))
+(defvar *med-tpinfo* (make-array +max-media-cnt+ :initial-contents (loop repeat +max-media-cnt+
+									    collecting (foreign-alloc '(:struct pjmedia-transport-info)))))
+(defvar *med-transport* (make-array +max-media-cnt+ :initial-element (null-pointer)))
+(defvar *sock-info* (make-array +max-media-cnt+ :initial-contents (loop repeat +max-media-cnt+
+									    collecting (foreign-alloc '(:struct pjmedia-sock-info)))))
+;;;Call variables
+(defvar *inv* (null-pointer))
+(defvar *med-stream* (null-pointer))
+;;(defvar *snd-port* (null-pointer))
+
+(defvar *mod-simpleua* (foreign-alloc '(:struct pjsip-module)))
+
