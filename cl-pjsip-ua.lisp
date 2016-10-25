58557a9f2af3f5c852e5271328b6672fc3b0d200
Eugene Zaikonnikov <eugene@funcall.org>
2016-10-25 21:42:45 +0200

    got rid of struct decls

--- cl-pjsip-ua.lisp
+++ cl-pjsip-ua.lisp
@@ -59,10 +59,10 @@
       (assert (pjlib-util-init))
       (pj-caching-pool-init *cp* (get-var-pointer *pj-pool-factory-default-policy*) 0)
       (let ((endpt-name (machine-instance)))
-	(assert-success (pjsip-endpt-create (foreign-slot-value *cp* '(:struct pj-caching-pool) 'factory) endpt-name *endpt*)))
-      (with-foreign-object (addr '(:union pj-sockaddr))
+	(assert-success (pjsip-endpt-create (foreign-slot-value *cp* 'pj-caching-pool 'factory) endpt-name *endpt*)))
+      (with-foreign-object (addr 'pj-sockaddr)
 	(pjsip-sockaddr-init *pj-af-inet* addr (null-pointer) +sip-port+) ;;ipv4
-	(assert-success (pjsip-udp-transport-start *endpt* (foreign-slot-value addr '(:union pj-sockaddr) 'ipv4)
+	(assert-success (pjsip-udp-transport-start *endpt* (foreign-slot-value addr 'pj-sockaddr 'ipv4)
 						   (null-pointer) 1 (null-pointer))))
       ;;init transaction layer
       (assert-success (pjsip-tsx-layer-init-module *endpt*))
@@ -70,9 +70,9 @@
       (assert-success (pjsip-ua-init-module *endpt* (null-pointer)))
 
       ;; init INVITE session module
-      (with-foreign-object (inv-cb '(:struct pjsip-inv-callback))
-	(pj-bzero inv-cb (foreign-type-size '(:struct pjsip-inv-callback)))
-	(with-foreign-slots ((on-state-changed on-new-session on-media-update) inv-cb (:struct pjsip-inv-callback))
+      (with-foreign-object (inv-cb 'pjsip-inv-callback)
+	(pj-bzero inv-cb (foreign-type-size 'pjsip-inv-callback))
+	(with-foreign-slots ((on-state-changed on-new-session on-media-update) inv-cb pjsip-inv-callback)
 	  (setf on-state-changed (callback 'call-on-state-changed)
 		on-new-session (callback 'call-on-forked)
 		on-media-update (callback 'call-on-media-update))
@@ -81,7 +81,7 @@
       (assert-success (pjsip-endpt-register-module *endpt* *mod-simpleua*))
 
       ;;init media endpoint
-      (assert-success (pjmedia-endpt-create (foreign-slot-value *cp* '(:struct pj-caching-pool) 'factory) (null-pointer) 1 *med-endpt*))
+      (assert-success (pjmedia-endpt-create (foreign-slot-value *cp* 'pj-caching-pool 'factory) (null-pointer) 1 *med-endpt*))
 
       ;;init G711
       (assert-success (pjmedia-codec-g711-init *med-endpt*))
@@ -93,24 +93,24 @@
 	   (pjmedia-transport-get-info (aref *med-transport* i) (aref *med-tpinfo* i))
 
 	   (foreign-funcall "memcpy" :pointer (mem-aref *sock-info* i)
-			    :pointer (foreign-slot-value (aref *med-tpinfo* i) '(:struct pjmedia-transport-info) 'sock-info)
-			    :int (foreign-type-size '(:struct pjmedia-sock-info))
+			    :pointer (foreign-slot-value (aref *med-tpinfo* i) 'pjmedia-transport-info 'sock-info)
+			    :int (foreign-type-size 'pjmedia-sock-info)
 			    :void))
       (if uri
-	  (with-foreign-objects ((hostaddr '(:union pj-sockaddr))
+	  (with-foreign-objects ((hostaddr 'pj-sockaddr)
 				 (dst-uri 'pj-str)
 				 (local-uri 'pj-str)
-				 (dlg '(:pointer (:struct pjsip-dialog)))
-				 (local-sdp '(:pointer (:struct pjmedia-sdp-session)))
-				 (tdata '(:pointer (:struct pjsip-tx-data))))
+				 (dlg '(:pointer pjsip-dialog))
+				 (local-sdp '(:pointer pjmedia-sdp-session))
+				 (tdata '(:pointer pjsip-tx-data)))
 	    (unless (pj-success (pj-gethostip *pj-af-inet* hostaddr))
 	      (format t "Unable to retrieve local host IP")
 	      (return-from run-agent nil))
 	    (lisp-string-to-pj-str (format nil "<sip:simpleuac@~A:~D>" (pj-str-to-lisp hostaddr) +sip-port+) local-uri)
 	    ;;create UAC dialog
 	    (assert-success (pjsip-dlg-create-uac (pjsip-ua-instance) local-uri local-uri dst-uri dst-uri dlg))
-	    (assert-success (pjmedia-endpt-create-sdp *med-endpt* (foreign-slot-value (mem-ref dlg '(:struct pjsip-dialog))
-										      '(:struct pjsip-dialog)'pool)
+	    (assert-success (pjmedia-endpt-create-sdp *med-endpt* (foreign-slot-value (mem-ref dlg 'pjsip-dialog)
+										      'pjsip-dialog'pool)
 						      +max-media-cnt+ *sock-info* local-sdp))
 
 	    ;;create the INVITE session and pass the above created SDP
@@ -122,9 +122,9 @@
 	  (format t "Ready to accept incoming calls.."))
       
       (loop until *complete* do
-	   (with-foreign-object (timeout '(:struct pj-time-val))
-	     (setf (foreign-slot-value timeout '(:struct pj-time-val) 'sec) 0
-		   (foreign-slot-value timeout '(:struct pj-time-val) 'msec) 10)
+	   (with-foreign-object (timeout 'pj-time-val)
+	     (setf (foreign-slot-value timeout 'pj-time-val 'sec) 0
+		   (foreign-slot-value timeout 'pj-time-val 'msec) 10)
 	     (pjsip-endpt-handle-events *endpt* timeout)))
 
       (unless (null-pointer-p *med-stream*)
@@ -142,7 +142,7 @@
 	(pjsip-endpt-destroy *endpt*))
 
       ;;release pool
-      (unless (null-pointer-p pool)
-	(pj-pool-release pool))))
+      (unless (null-pointer-p pool-ptr)
+	(pj-pool-release pool-ptr))))
   t)
 
