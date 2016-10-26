18c20140a74a65fbe572f44bc9bc495c80ffa8ad
Eugene Zaikonnikov <eugene@funcall.org>
2016-10-26 14:28:49 +0200

    comment out pj-str stuff for now

--- cl-pjsip.lisp
+++ cl-pjsip.lisp
@@ -63,30 +63,30 @@
 		   (foreign-slot-value pointer 'pj-str 'ptr) 0 new-end string 0)
 	  string)))))
 
-(define-foreign-type pj-str-type ()
-  (;; CFFI encoding of this string.
-   (encoding :initform nil :initarg :encoding :reader encoding)
-   ;; Should we free after translating from foreign?
-   (free-from-foreign :initarg :free-from-foreign
-                      :reader fst-free-from-foreign-p
-                      :initform nil :type boolean)
-   ;; Should we free after translating to foreign?
-   (free-to-foreign :initarg :free-to-foreign
-                    :reader fst-free-to-foreign-p
-                    :initform t :type boolean))
-  (:actual-type pj-str)
-  ;;(:simple-parser :string)
-  )
+;; (define-foreign-type pj-str-tclass ()
+;;   (;; CFFI encoding of this string.
+;;    (encoding :initform nil :initarg :encoding :reader encoding)
+;;    ;; Should we free after translating from foreign?
+;;    (free-from-foreign :initarg :free-from-foreign
+;;                       :reader fst-free-from-foreign-p
+;;                       :initform nil :type boolean)
+;;    ;; Should we free after translating to foreign?
+;;    (free-to-foreign :initarg :free-to-foreign
+;;                     :reader fst-free-to-foreign-p
+;;                     :initform t :type boolean))
+;;   (:actual-type pj-str)
+;;   ;;(:simple-parser :string)
+;;   )
 
-(defmethod translate-to-foreign ((s string) (type pj-str-type))
-  (values (foreign-string-alloc s :encoding (fst-encoding type))
-          (fst-free-to-foreign-p type)))
+;; (defmethod translate-to-foreign ((s string) (type pj-str-tclass))
+;;   (values (lisp-string-to-pj-str s (foreign-alloc 'pj-str))
+;;           (fst-free-to-foreign-p type)))
 
-(defmethod translate-from-foreign (ptr (type pj-str-type))
-  (unwind-protect
-       (values (pj-str-to-lisp ptr))
-    (when (fst-free-from-foreign-p type)
-      (foreign-free ptr))))
+;; (defmethod translate-from-foreign (ptr (type pj-str-tclass))
+;;   (unwind-protect
+;;        (values (pj-str-to-lisp ptr))
+;;     (when (fst-free-from-foreign-p type)
+;;       (foreign-free ptr))))
 
 (defcenum pjsip-module-priority
   (:pjsip-mod-priority-transport-layer 8)
