b1eb8484e66e4ba90e721ca1eaac187207be2d6c
Eugene Zaikonnikov <eugene@funcall.org>
2016-12-02 13:20:19 +0100

    error handling..

--- cl-pjsua-demo.lisp
+++ cl-pjsua-demo.lisp
@@ -2,7 +2,29 @@
 
 (defvar *sip-domain* "example.com")
 (defvar *sip-user* "alice")
-(defvar *spi-passwd* "secret")
+(defvar *sip-passwd* "secret")
+
+(defmacro assert-no-error (form error-message)
+  (let ((retval (gentemp)))
+    `(let ((,retval ,form))
+       (unless (pj-success ,retval)
+	 (error-exit ,error-message ,retval)))))
+
+(defun error-exit (title status)
+  (pjsua-perror "cl-pjsua-demo" title status)
+  (pjsua-destroy)
+  (throw 'demo-error nil))
 
 (defun run-pjsua (&optional destination)
-  (use-foreign-library libpjsua))
+  (use-foreign-library libpjsua)
+
+  (assert-no-error (pjsua-create) "Error in pjsua_create()")
+  
+  (when destination
+    (assert-no-error (pjsua-verify-url destination) "Invalid URL as argument"))
+  
+  (with-foreign-objects ((cfg 'pjsua-config) (log-cfg 'pjsua-logging-config))
+    )
+  
+  (catch 'demo-error
+    (ua-log "Exiting")))
