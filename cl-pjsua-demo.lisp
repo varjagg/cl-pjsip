eea4f72e661975094f99c30f6a22e1902e5fc69e
Eugene Zaikonnikov <eugene@funcall.org>
2016-12-02 14:04:11 +0100

    expending the demo

--- cl-pjsua-demo.lisp
+++ cl-pjsua-demo.lisp
@@ -24,7 +24,16 @@
     (assert-no-error (pjsua-verify-url destination) "Invalid URL as argument"))
   
   (with-foreign-objects ((cfg 'pjsua-config) (log-cfg 'pjsua-logging-config))
-    )
+    (pjsua-config-default cfg)
+    (with-foreign-slots ((on-incoming-call on-call-media-state on-call-state) (foreign-slot-value cfg 'pjsua-config 'cb) pjsua-callback)
+      (setf on-incoming-call (callback on-incoming-call)
+	    on-call-media-state (callback on-call-media-state)
+	    on-call-state (callback on-call-state)))
+    
+    (pjsua-logging-config-default log-cfg)
+    (with-foreign-slots ((console-level cb) log-cfg pjsua-logging-config)
+      (setf console-level 4
+	    cb (callback logger))))
   
   (catch 'demo-error
     (ua-log "Exiting")))
